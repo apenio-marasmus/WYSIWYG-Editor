f344cd66e7f8b6f7053a8f543b781a0a26eec91b
martin.asmus <martin.asmus@apenio.de>
2026-08-14 15:15:54 +0200

    Rename references from Apenio Office to Apenio WYSIWYG across various files, including UI translations, socket handling, and documentation. Update product names in configuration files and Docker metadata to reflect the new branding. Ensure consistency in user-facing messages and internal references to enhance clarity and branding alignment.

--- browser/src/app/Socket.ts
+++ browser/src/app/Socket.ts
@@ -324,7 +324,7 @@ class Socket {
 							'{productname}',
 							typeof brandProductName !== 'undefined'
 								? brandProductName
-								: 'Apenio Office',
+								: 'Apenio WYSIWYG',
 						) + e,
 					cmd: 'socket',
 					kind: 'failed',
@@ -2067,7 +2067,7 @@ class Socket {
 						'{productname}',
 						typeof brandProductName !== 'undefined'
 							? brandProductName
-							: 'Apenio Office',
+							: 'Apenio WYSIWYG',
 					);
 					msg = msg.replace('{0}', window.expectedServerId);
 					msg = msg.replace('{1}', window.routeToken);
@@ -2727,7 +2727,7 @@ class Socket {
 				'{productname}',
 				typeof brandProductName !== 'undefined'
 					? brandProductName
-					: 'Apenio Office',
+					: 'Apenio WYSIWYG',
 			);
 			this._map.fire('infobar', {
 				msg: textMsg,
